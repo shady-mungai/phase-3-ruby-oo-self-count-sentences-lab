7dec4215acae91a257197724ce3fcdda691d263d
shady-mungai <shadrackmungai10@gmail.com>
2023-02-12 23:48:50 +0300

    Completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
-require 'pry'
+#require 'pry'
 
 class String
 
   def sentence?
-    
-  end
+    self[-1] == "."
+
+  end  
+  
+
 
   def question?
+    self[-1] == "?"
 
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).delete_if { |word| word.size < 2 }.size
   end
 end
+
